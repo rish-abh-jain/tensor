a4446d1b01a327c2c00310da49a0f11cfedf47b8
rish-abh-jain <60362872+rish-abh-jain@users.noreply.github.com>
2020-05-24 22:12:41 +0530

    grg

---
+++ cnn.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.applications import VGG16
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import RMSprop
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+# VGG16 was designed to work on 224 x 224 pixel input images sizes
+img_rows = 224
+img_cols = 224
+
+#Loads the VGG16 model
+model = VGG16(weights = 'imagenet', 
+                 include_top = False, 
+                 input_shape = (img_rows, img_cols, 3))
+for layer in model.layers:
+    layer.trainable = False
+    
+# Let's print our layers 
+for (i,layer) in enumerate(model.layers):
+    print(str(i) + " "+ layer.__class__.__name__, layer.trainable)
+def addTopModel(bottom_model, num_classes, D=256):
+    """creates the top or head of the model that will be 
+    placed ontop of the bottom layers"""
+    top_model = bottom_model.output
+    top_model = Flatten(name = "flatten")(top_model)
+    top_model = Dense(D, activation = "relu")(top_model)
+    top_model = Dense(D, activation = "relu")(top_model)
+    top_model = Dropout(0.3)(top_model)
+    top_model = Dense(num_classes, activation = "softmax")(top_model)
+    return top_model
+num_classes = 2
+
+FC_Head = addTopModel(model, num_classes)
+
+modelnew = Model(inputs=model.input, outputs=FC_Head)
+
+print(modelnew.summary())
+train_data_dir = 'Datasets//Datasets//Train//'
+validation_data_dir = 'Datasets//Datasets//validation//'
+
+train_datagen = ImageDataGenerator(
+      rescale=1./255,
+      rotation_range=20,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+ 
+validation_datagen = ImageDataGenerator(rescale=1./255)
+ 
+# Change the batchsize according to your system RAM
+train_batchsize = 32
+val_batchsize = 10
+ 
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=train_batchsize,
+        class_mode='categorical')
+ 
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_rows, img_cols),
+        batch_size=val_batchsize,
+        class_mode='categorical',
+        shuffle=False)
+checkpoint = ModelCheckpoint("faces.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+
+earlystop = EarlyStopping(monitor = 'val_loss', 
+                          min_delta = 0, 
+                          patience = 3,
+                          verbose = 1,
+                          restore_best_weights = True)
+
+# we put our call backs into a callback list
+callbacks = [earlystop, checkpoint]
+
+# Note we use a very small learning rate 
+modelnew.compile(loss = 'categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.001),
+              metrics = ['accuracy'])
+
+nb_train_samples = 1000
+nb_validation_samples = 200
+epochs = 1
+batch_size = 32
+
+history = modelnew.fit_generator(
+    train_generator,
+    steps_per_epoch = nb_train_samples // batch_size,
+    epochs = epochs,
+    callbacks = callbacks,
+    validation_data = validation_generator,
+    validation_steps = nb_validation_samples // batch_size)
+
+modelnew.save("faces.h5")
+
+
+# In[ ]:
+
+
+
+
