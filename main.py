f194398be96b6e6fad0e8a35148f3ffd427ada69
rish-abh-jain <60362872+rish-abh-jain@users.noreply.github.com>
2020-05-25 00:46:48 +0530

    Update main.py
    szdzsdsdssf

--- main.py
+++ main.py
@@ -1,104 +1,37 @@
-from keras.applications import VGG16
+# Import essentials
+import keras
+from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import ConvD, MaxPooling2D, ZeroPadding2D
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers.normalization import BatchNormalization
-from keras.models import Model
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import RMSprop
-from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.utils.np_utils import to_categorical
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.backend import clear_session
+import numpy
 
-# VGG16 was designed to work on 224 x 224 pixel input images sizes
-img_rows = 224
-img_cols = 224
+# Load Model 
+(train_X , train_y), (test_X , test_y) = mnist.load_data("mymnist.data")
+# Reshape data and change type
+test_X = test_X.reshape(-1 , 28*28)
+train_X = train_X.reshape(-1 ,  28*28)
+test_X = test_X.astype("float32")
+train_X = train_X.astype("float32")
+# One hot encoding 
+test_y = to_categorical(test_y)
+train_y = to_categorical(train_y)
 
-#Loads the VGG16 model
-model = VGG16(weights = 'imagenet', 
-                 include_top = False, 
-                 input_shape = (img_rows, img_cols, 3))
-for layer in model.layers:
-    layer.trainable = False
-    
-# Let's print our layers 
-for (i,layer) in enumerate(model.layers):
-    print(str(i) + " "+ layer.__class__.__name__, layer.trainable)
-def addTopModel(bottom_model, num_classes, D=256):
-    """creates the top or head of the model that will be 
-    placed ontop of the bottom layers"""
-    top_model = bottom_model.output
-    top_model = Flatten(name = "flatten")(top_model)
-    top_model = Dense(D, activation = "relu")(top_model)
-    top_model = Dense(D, activation = "relu")(top_model)
-    top_model = Dropout(0.3)(top_model)
-    top_model = Dense(num_classes, activation = "softmax")(top_model)
-    return top_model
-num_classes = 2
+model = Sequential()
+model.add(Dense(units = 20 , input_dim = 28*28 , activation = 'relu'))
+model.add(Dense(units=200 , input_dim = 28*28 , activation = 'relu'))
+model.add(Dense(units=60 , input_dim = 28*28 , activation = 'relu'))
+model.add(Dense(units=10 , input_dim = 28*28 , activation = 'softmax'))
+model.compile( optimizer= "Adam" , loss='categorical_crossentropy', 
+             metrics=['accuracy'] )
+fit_model = model.fit(train_X ,  train_y , epochs = 2 , verbose =  False)
 
-FC_Head = addTopModel(model, num_classes)
-
-modelnew = Model(inputs=model.input, outputs=FC_Head)
-
-print(modelnew.summary())
-train_data_dir = 'DATA//train//'
-validation_data_dir = 'DATA//test//'
-
-train_datagen = ImageDataGenerator(
-      rescale=1./255,
-      rotation_range=20,
-      width_shift_range=0.2,
-      height_shift_range=0.2,
-      horizontal_flip=True,
-      fill_mode='nearest')
- 
-validation_datagen = ImageDataGenerator(rescale=1./255)
- 
-# Change the batchsize according to your system RAM
-train_batchsize = 32
-val_batchsize = 10
- 
-train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_rows, img_cols),
-        batch_size=train_batchsize,
-        class_mode='categorical')
- 
-validation_generator = validation_datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_rows, img_cols),
-        batch_size=val_batchsize,
-        class_mode='categorical',
-        shuffle=False)
-checkpoint = ModelCheckpoint("faces.h5",
-                             monitor="val_loss",
-                             mode="min",
-                             save_best_only = True,
-                             verbose=1)
-
-earlystop = EarlyStopping(monitor = 'val_loss', 
-                          min_delta = 0, 
-                          patience = 3,
-                          verbose = 1,
-                          restore_best_weights = True)
-
-# we put our call backs into a callback list
-callbacks = [earlystop, checkpoint]
-
-# Note we use a very small learning rate 
-modelnew.compile(loss = 'categorical_crossentropy',
-              optimizer = RMSprop(lr = 0.001),
-              metrics = ['accuracy'])
-
-nb_train_samples = 1000
-nb_validation_samples = 200
-epochs = 1
-batch_size = 32
-
-history = modelnew.fit_generator(
-    train_generator,
-    steps_per_epoch = nb_train_samples // batch_size,
-    epochs = epochs,
-    callbacks = callbacks,
-    validation_data = validation_generator,
-    validation_steps = nb_validation_samples // batch_size)
-
-modelnew.save("faces.h5")
+text = fit_model.history
+accuracy = text['accuracy'][1] * 100
+accuracy = int(accuracy)
+f= open("accuracy.txt","w+")
+f.write(str(accuracy))
+f.close()
+print("Accuracy for the model is : " , accuracy ,"%")
